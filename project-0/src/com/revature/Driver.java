edcc83b51c4917d61164cca578ce99bb798b650b
jazibnasim <jazib.nasim@gmail.com>
2022-03-02 14:51:25 -0500

    added project-0 classes

---
+++ project-0/src/com/revature/Driver.java
@@ -0,0 +1,10 @@
+package com.revature;
+
+public class Driver {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello!");
+    }
+}
---
+++ project-0/src/com/revature/User.java
@@ -0,0 +1,10 @@
+package com.revature;
+
+public class User {
+
+    public String userName = "";
+    public String password = "";
+
+    
+
+}
