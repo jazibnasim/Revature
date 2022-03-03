7e9968a2a5fee5d72d6e7ef34d5de187b8c05850
jazibnasim <jazib.nasim@gmail.com>
2022-03-02 19:03:17 -0500

    added recent driver logic

---
+++ project-0/src/com/revature/Account.java
@@ -0,0 +1,8 @@
+package com.revature;
+
+public class Account {
+
+    private double bal = 0;
+    private double deposit;
+    private double withdraw;
+}
--- project-0/src/com/revature/User.java
+++ project-0/src/com/revature/User.java
@@ -1,10 +1,48 @@
 package com.revature;
 
+
+
+
 public class User {
 
-    public String userName = "";
-    public String password = "";
+    private String userName = "";
+    private String password = "";
+    private String firstName = "";
+    private String lastName = "";
 
-    
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void login(){
+
+    }
 }
